63fe5561ca36c4bb62e8085cbd479558b3979e9f
Andy <gabryelb027@gmail.com>
2025-04-03 23:26:02 -0500

    Semana no se cual es: 2do Intento de practica "se logro" es una version primitiva pero logrado

--- app/src/main/java/com/example/testeo_proyecto/MainActivity.java
+++ app/src/main/java/com/example/testeo_proyecto/MainActivity.java
@@ -21,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);//define que layout se va a usar
 
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
 
 
 
--- app/src/main/java/com/example/testeo_proyecto/SecondActivity.java
+++ app/src/main/java/com/example/testeo_proyecto/SecondActivity.java
@@ -17,6 +17,8 @@ import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
+    String N;
+    Integer Num;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,12 +30,24 @@ public class SecondActivity extends AppCompatActivity {
             return insets;
         });
 
+        Contacto contact = new Contacto(N, Num);
+        Contacto contact1 = new Contacto(N, Num);
+        Contacto contact2 = new Contacto(N, Num);
+        Contacto contact3 = new Contacto(N, Num);
+        contact.nombre = "Jose Luis";
+        contact.num = 123456789;
+        contact1.nombre = "Jose Manuel";
+        contact1.num = 789456123;
+        contact2.nombre = "Jose Reyes";
+        contact2.num = 987123456;
+        contact3.nombre = "Jose Bazan";
+        contact3.num = 321456987;
+
         List<String> data = new ArrayList<>();
-        data.add("dato0");
-        data.add("dato1");
-        data.add("dato2");
-        data.add("dato3");
-        data.add("dato3");
+        data.add(contact.nombre + "\\\n"+contact.num);
+        data.add(contact1.nombre + "\\\n"+contact1.num);
+        data.add(contact2.nombre + "\\\n"+contact2.num);
+        data.add(contact3.nombre + "\\\n"+contact3.num);
 
         BasicAdapter basicAdapter = new BasicAdapter(data);
         RecyclerView rvBasic = findViewById(R.id.rvBasic);
@@ -44,5 +58,11 @@ public class SecondActivity extends AppCompatActivity {
     {
         String nombre;
         Integer num;
+
+        public Contacto(String nombre, Integer num)
+        {
+            this.nombre=nombre;
+            this.num =num;
+        }
     }
 }
